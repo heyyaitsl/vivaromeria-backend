c896aa1dade1e0a50a7a1a751d5f25079b3670cd
laura <laurasarmiento1996@gmail.com>
2023-12-30 13:17:49 +0000

    established database connection

---
+++ romerias/src/main/java/tfg/romerias/RomeriasApplication.java
@@ -0,0 +1,13 @@
+package tfg.romerias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomeriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomeriasApplication.class, args);
+	}
+
+}
---
+++ romerias/src/test/java/tfg/romerias/RomeriasApplicationTests.java
@@ -0,0 +1,13 @@
+package tfg.romerias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomeriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
