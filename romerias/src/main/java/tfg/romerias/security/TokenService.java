bd96617e3dc3d71b8d35b76f9cd02554fdaab389
laura <laurasarmiento1996@gmail.com>
2024-12-01 13:59:13 +0000

    added expiration date to token

--- romerias/src/main/java/tfg/romerias/security/TokenService.java
+++ romerias/src/main/java/tfg/romerias/security/TokenService.java
@@ -8,12 +8,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
 public class TokenService {
 
     private final SecretKey secretKey;
+    private static final long EXPIRATION_TIME = 3600000;
 
     public TokenService() {
         this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512); // Clave segura de 512 bits
@@ -24,6 +26,8 @@ public class TokenService {
         return Jwts.builder()
                 .setSubject(username)
                 .claim("role", role)
+                .setIssuedAt(new Date()) // Fecha de creación del token
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) // Fecha de expiración
                 .signWith(secretKey)
                 .compact();
     }
@@ -45,7 +49,12 @@ public class TokenService {
     // Validar el token JWT
     public boolean validateToken(String token, UserDetails userDetails) {
         return extractClaims(token)
-                .map(claims -> claims.getSubject().equals(userDetails.getUsername()))
+                .map(claims -> {
+                    String username = claims.getSubject();
+                    Date expiration = claims.getExpiration();
+                    return username.equals(userDetails.getUsername())&& expiration.after(new Date());
+                    //claims.getSubject().equals(userDetails.getUsername())
+                })
                 .orElse(false);
     }
 }
